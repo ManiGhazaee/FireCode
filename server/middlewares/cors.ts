7c0f1ddf2ef57ff146e72c663e5fa5cded51dfb1
ManiGhazaee <hosseinghazaee1@gmail.com>
2023-08-14 21:00:56 +0330

    handle preflight cors

--- server/middlewares/cors.ts
+++ server/middlewares/cors.ts
@@ -7,5 +7,20 @@ export function customCors(
     next: NextFunction
 ) {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "ture");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, OPTIONS, DELETE"
+    );
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).json({
+            body: "OK",
+        });
+    }
     next();
 }
